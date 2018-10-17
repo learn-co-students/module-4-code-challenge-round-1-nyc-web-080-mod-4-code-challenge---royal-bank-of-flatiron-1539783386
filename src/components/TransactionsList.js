620066deb519f1657bf0fb0dccc1f901cc3a8923
Richard Chen <richard.c.chen@gmail.com>
2018-10-17 10:50:54 -0400

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
-
+const TransactionsList = (props) => {
+  //console.log("translist props:", props);
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -28,7 +29,14 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {props.showInfo.map(trans => {
+          return <Transaction trans={trans} />
+        })
+        }
+        {props.showInfo2.map(trans => {
+          return <Transaction trans={trans} />
+        })
+        }
 
       </tbody>
     </table>
