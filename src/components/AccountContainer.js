c8ce00e08be5b6dfefe29099563723abf17bfad4
kristinatong <kristina.tong@gmail.com>
2018-10-17 10:12:12 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,6 +7,11 @@ class AccountContainer extends Component {
 
   constructor() {
     super()
+    this.state = {
+      allTransactions: transactions,
+      filteredTransactions: transactions,
+      searchBy: ""
+    }
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
@@ -14,16 +19,34 @@ class AccountContainer extends Component {
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount(){
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions').then(res => res.json())
+      .then(data => this.setState({allTransactions:data,filterTransactions:data}))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchBy: event.target.value
+    })
+    this.filterTransactions(event.target.value)
+  }
+
+  filterTransactions = (filterBy) => {
+    const filteredTransactions = this.state.allTransactions.filter(transaction => {
+      return transaction.category.toLowerCase().includes(filterBy) || transaction.description.toLowerCase().includes(filterBy)
+    })
+
+    this.setState({
+      filteredTransactions: filteredTransactions
+    })
   }
 
   render() {
-
+    console.log(this.state)
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange}/>
+        <TransactionsList transactions={this.state.filteredTransactions}/>
       </div>
     )
   }
