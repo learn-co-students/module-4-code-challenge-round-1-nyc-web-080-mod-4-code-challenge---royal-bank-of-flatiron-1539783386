c7e98864ce646b2f856b6e60000c903e45dd5895
ParkMD7 <parkermdinsmore@gmail.com>
2018-10-17 11:02:54 -0400

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+// Deliverable #1 - Step #4 - verify props were passed correctly
+const TransactionsList = (props) => {
+// console.log(props.transactions);
+console.log(props.searchInput);
 
+// Deliverable #1 - Step #5 - Map through transactions & pass them down as props to each Transaction
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -28,10 +33,33 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
-
-      </tbody>
+        {props.transactions.map(transaction => {
+          if(props.searchInput == ''){
+            return (
+              <Transaction
+                key={transaction.id}
+                posted_at={transaction.posted_at}
+                description={transaction.description}
+                category={transaction.category}
+                amount={transaction.amount}
+              />
+            )
+          }
+          else if(transaction.category.includes(props.searchInput) || transaction.description.includes(props.searchInput)){
+            return (
+              <Transaction
+                key={transaction.id}
+                posted_at={transaction.posted_at}
+                description={transaction.description}
+                category={transaction.category}
+                amount={transaction.amount}
+              />
+            )
+          }
+        })}
+            </tbody>
     </table>
+
   )
 }
 
